b1ba365256db5b518c3cab18f5321e19db165395
Dave Anski <dave.anski@gmail.com>
2026-01-29 20:10:08 +0000

    Add ability to input time in minutes for disabling Pihole

--- pihole_disable.py
+++ pihole_disable.py
@@ -1,6 +1,7 @@
 import os
 from typing import Any
 import json
+import argparse
 
 import requests
 from dotenv import load_dotenv
@@ -8,6 +9,19 @@ from dotenv import load_dotenv
 URL_API = "https://pihole.dacyho.me/api"
 AUTH_PATH = "/auth"
 DNS_PATH = "/dns/blocking"
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "period",
+        nargs="?",
+        type=int,
+        default=1,
+        help="how many minutes to disable Pihole",
+    )
+
+    return parser.parse_args()
 
 
 def authenticate(_api_pass: str) -> dict[str, Any]:
@@ -36,12 +50,13 @@ def logout(_sid: str) -> None:
 
 
 if __name__ == "__main__":
+    args = parse_args()
     load_dotenv(".env")
     api_pass = os.environ.get("API_PASS")
 
     response = authenticate(api_pass)
     sid = response["session"]["sid"]
 
-    disable_blocking(sid, 60)
+    disable_blocking(sid, args.period)
 
     logout(sid)
