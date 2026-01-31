52546dc51c19ae2e8bec73f65a3064cf4bceeecf
Dave Anski <dave.anski@gmail.com>
2026-01-31 16:55:10 +0000

    Revert "Precede running the Python script via bash script with a nohup command"
    This reverts commit 176162bffa8c1123aab736b55b2c32c9e13cddb6.

--- run_pihole_disable.sh
+++ run_pihole_disable.sh
@@ -14,7 +14,7 @@ fi
 echo "Activating Python environment"
 source .venv/bin/activate
 echo "Starting script"
-nohup python "$APP" > .pihole_disable.log 2> .pihole_disable.err &
+python "$APP" > .pihole_disable.log 2> .pihole_disable.err &
 echo "Script running in background"
 echo -e "Use ./stop_pihole_disable.sh to terminate\n"
 exit 0
