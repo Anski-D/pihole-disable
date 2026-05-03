5b5775c88cc07e9b403c8de03f083403a3be631e
Dave Anski <dave.anski@gmail.com>
2026-05-03 17:18:57 +0100

    Move routing to Flask and in new module app.py

---
+++ app.py
@@ -0,0 +1,53 @@
+import os
+
+from dotenv import load_dotenv
+from flask import Flask, render_template, redirect, url_for, request
+
+from pihole_disable import PiholeDisabler
+
+ENV_FILE = ".env"
+load_dotenv(ENV_FILE)
+API_PASSWORD = os.environ["API_PASSWORD"]
+disabler = PiholeDisabler(API_PASSWORD)
+
+app = Flask(__name__)
+
+
+def _clean_period_value(period: float) -> float:
+    return max(0, period)
+
+
+@app.route("/")
+def index():
+    blocked = disabler.check_blocking()
+    blocked["timer"] = round(blocked["timer"])
+
+    return render_template(
+        "index.html",
+        blocked=blocked,
+        refresh_period=5,
+    )
+
+
+@app.route("/enable")
+def enable():
+    disabler.enable_blocking()
+
+    return redirect(url_for("index"))
+
+
+@app.route("/disable", methods=("POST", "GET"))
+@app.route("/disable/<period>")
+def disable(period: str=""):
+    if request.method == "POST":
+        if period := _clean_period_value(float(request.form["period"])) > 0:
+            disabler.disable_blocking(period)
+
+        return redirect(url_for("index"))
+
+    if period:
+        disabler.increase_disable_period(_clean_period_value(int(period)))
+
+        return redirect(url_for("index"))
+
+    return render_template("disable.html", refresh_period=0)
