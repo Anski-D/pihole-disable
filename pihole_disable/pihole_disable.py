2003812e794bbec1ca2ea5c59a553c8556c38ac0
Dave Anski <dave.anski@gmail.com>
2026-05-21 22:09:23 +0100

    Move authentication logic into its own AuthManager class

--- pihole_disable/pihole_disable.py
+++ pihole_disable/pihole_disable.py
@@ -1,6 +1,7 @@
 import json
 from functools import wraps
 import logging
+from typing import Protocol
 
 import requests
 import urllib3
@@ -11,7 +12,7 @@ urllib3.disable_warnings()
 
 def requires_auth(func):
     @wraps(func)
-    def check_auth(obj, *args, **kwargs):
+    def check_auth(obj: Authable, *args, **kwargs):
         if not obj.is_authenticated:
             log.info("%s not authenticated", obj)
             obj.authenticate()
@@ -21,20 +22,26 @@ def requires_auth(func):
     return check_auth
 
 
-class PiholeDisabler:
+class Authable(Protocol):
+    is_authenticated: bool
+
+    def authenticate(self) -> None: ...
+
+
+class AuthManager:
     _auth_path = "/auth"
-    _dns_path = "/dns/blocking"
 
     def __init__(self, api_url: str, password: str) -> None:
-        self._api_url = api_url
+        self.api_url = api_url
         self._password = password
         log.info("Created %s", self)
+        self._url = self.api_url + self._auth_path
         self._sid = ""
         self._csrf = ""
         self.authenticate()
 
     def __repr__(self) -> str:
-        return f"{type(self).__name__}({self._api_url}, {type(self._password)})"
+        return f"{type(self).__name__}({self.api_url}, {type(self._password)})"
 
     @property
     def headers(self) -> dict[str, str]:
@@ -42,25 +49,51 @@ class PiholeDisabler:
 
     @property
     def is_authenticated(self) -> bool:
-        url = f"{self._api_url}{self._auth_path}"
-        response = requests.request("GET", url, headers=self.headers, verify=False)
+        response = requests.request("GET", self._url, headers=self.headers, verify=False)
 
         return response.status_code == 200
 
     def authenticate(self) -> None:
         log.info("Authenticating %s with Pihole API", self)
-        url = f"{self._api_url}{self._auth_path}"
         payload = {"password": self._password}
-        response = json.loads(requests.request("POST", url, json=payload, verify=False).text)
+        response = json.loads(requests.request("POST", self._url, json=payload, verify=False).text)
         self._sid = response["session"]["sid"]
         self._csrf = response["session"]["csrf"]
 
     @requires_auth
+    def logout(self) -> None:
+        log.info("Logging out of Pihole API")
+        requests.request("DELETE", self._url, headers=self.headers, verify=False)
+
+
+
+class PiholeDisabler:
+    _dns_path = "/dns/blocking"
+
+    def __init__(self, auth_manager: AuthManager) -> None:
+        self._auth_manager = auth_manager
+        log.info("Created %s", self)
+        self._url = self._auth_manager.api_url + self._dns_path
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}({self._auth_manager})"
+
+    @property
+    def headers(self) -> dict[str, str]:
+        return self._auth_manager.headers
+
+    @property
+    def is_authenticated(self) -> bool:
+        return self._auth_manager.is_authenticated
+
+    def authenticate(self) -> None:
+        self._auth_manager.authenticate()
+
+    @requires_auth
     def check_blocking(self) -> dict[str, bool | int]:
         log.debug("Checking blocking status")
-        url = f"{self._api_url}{self._dns_path}"
 
-        response = json.loads(requests.request("GET", url, headers=self.headers, json={}, verify=False).text)
+        response = json.loads(requests.request("GET", self._url, headers=self.headers, json={}, verify=False).text)
 
         return {
             "blocking": response["blocking"] == "enabled",
@@ -70,13 +103,12 @@ class PiholeDisabler:
     @requires_auth
     def disable_blocking(self, period: float) -> None:
         log.info("Disabling blocking for %s minutes", round(period))
-        url = f"{self._api_url}{self._dns_path}"
         payload = {
             "blocking": False,
             "timer": 60 * period, # Period in minutes converted to seconds
         }
 
-        requests.request("POST", url, headers=self.headers, json=payload, verify=False)
+        requests.request("POST", self._url, headers=self.headers, json=payload, verify=False)
 
     def increase_disable_period(self, period: float) -> None:
         current_status = self.check_blocking()
@@ -90,16 +122,8 @@ class PiholeDisabler:
     @requires_auth
     def enable_blocking(self) -> None:
         log.info("Enabling blocking")
-        url = f"{self._api_url}{self._dns_path}"
         payload = {
             "blocking": True,
         }
 
-        requests.request("POST", url, headers=self.headers, json=payload, verify=False)
-
-    @requires_auth
-    def logout(self) -> None:
-        log.info("Logging out of Pihole API")
-        url = f"{self._api_url}{self._auth_path}"
-
-        requests.request("DELETE", url, headers=self.headers, verify=False)
+        requests.request("POST", self._url, headers=self.headers, json=payload, verify=False)
