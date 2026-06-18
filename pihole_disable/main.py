0c0bb819872346bcbe6a8ed5d81fb4be1c4a8100
Dave Anski <dave.anski@gmail.com>
2026-06-18 20:30:57 +0100

    Move code from package __init__ to main module to better control how the package loads

---
+++ pihole_disable/main.py
@@ -0,0 +1,36 @@
+import logging
+import logging.config
+import os
+
+import yaml
+from quart import Quart
+from dotenv import load_dotenv
+
+LOGGING_CONFIG = "logging.yaml"
+ENV_FILE = ".env"
+load_dotenv(ENV_FILE)
+API_URL = os.environ["API_URL"]
+API_PASSWORD = os.environ["API_PASSWORD"]
+
+
+def _setup_logging() -> logging.Logger:
+    with open(LOGGING_CONFIG) as f:
+        config = yaml.safe_load(f)
+
+    logging.config.dictConfig(config)
+
+    return logging.getLogger(__name__)
+
+
+log = _setup_logging()
+
+
+def create_app() -> Quart:
+    log.info("Creating Flask app")
+
+    return Quart(__name__)
+
+
+app = create_app()
+
+import pihole_disable.views
