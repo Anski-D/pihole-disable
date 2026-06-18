2eb6cd27d75ae96caf15de0fdbf92530be8e58ef
Dave Anski <dave.anski@gmail.com>
2026-06-18 20:32:24 +0100

    Update structure of test files and tweak config of pytest

---
+++ tests/test_utils.py
@@ -0,0 +1,26 @@
+import pytest
+
+from pihole_disable.utils import _clean_period_value, _check_client
+
+
+@pytest.mark.parametrize("value_in, value_out", [(-1, 0), (1, 1), (10000, 10000), ("-1", 0), ("1", 1), ("10000", 10000)])
+def test_clean_period_value(value_in: int | str, value_out: int) -> None:
+    assert _clean_period_value(value_in) == value_out
+
+
+class TestCheckClient:
+    @pytest.mark.parametrize("value_in, bool_out", [("111.111.111.111", True), ("111.111.111.1111", False)])
+    def test_check_client_len(self, value_in: str, bool_out: bool) -> None:
+        assert _check_client(value_in) == bool_out
+
+    @pytest.mark.parametrize("value_in, bool_out", [("1.1.1.1", True), ("1.1.1", False), ("1.1.1.1.1", False)])
+    def test_check_client_parts_count(self, value_in: str, bool_out: bool) -> None:
+        assert _check_client(value_in) == bool_out
+
+    @pytest.mark.parametrize("value_in, bool_out", [("1.2.32.10", True), ("1.2.34.xxx", False), ("x.x.x.x", False)])
+    def test_check_client_part_is_digit(self, value_in: str, bool_out: bool) -> None:
+        assert _check_client(value_in) == bool_out
+
+    @pytest.mark.parametrize("value_in, bool_out", [("0.0.0.0", True), ("255.255.255.255", True), ("1.1.1.300", False)])
+    def test_check_client_part_value_in_range(self, value_in: str, bool_out: bool) -> None:
+        assert _check_client(value_in) == bool_out
--- tests/test_views.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-from pihole_disable.views import _clean_period_value
-
-class TestCleanPeriodValue:
-    def test_clean_period_value_negative(self) -> None:
-        assert _clean_period_value(-1) == 0
